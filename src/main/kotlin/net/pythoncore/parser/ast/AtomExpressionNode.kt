98c9664b9309a56741afabb473ab005f32390e79
Richard Magnor Stenbro <stenbror@hotmail.com>
2022-02-12 13:15:08 +0100

    Added nodes for atom expressions.

---
+++ src/main/kotlin/net/pythoncore/parser/ast/AtomExpressionNode.kt
@@ -0,0 +1,11 @@
+package net.pythoncore.parser.ast
+
+import net.pythoncore.parser.Token
+
+class AtomExpressionNode(startPos: Int, endPos: Int, await: Boolean, symbol: Token, left: BaseNode, trailers: Array<BaseNode>)
+    : BaseNode(startPos, endPos) {
+        val isAwait = await
+        val symbolOne = symbol
+        val leftNode = left
+        val trailerNodes = trailers
+}
