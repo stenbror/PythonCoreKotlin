a7f2085d4b36184ace687fbbf7744744ea9f4240
Richard Magnor Stenbro <stenbror@hotmail.com>
2022-03-06 17:12:12 +0100

    UnitTests for set with star expr single element

--- src/test/kotlin/net/pythoncore/tests/ParserTest.kt
+++ src/test/kotlin/net/pythoncore/tests/ParserTest.kt
@@ -659,4 +659,33 @@ class ParserTest {
         assertEquals(0, (node as EvalInputNode).newlineNode.size)
         assertEquals(TokenCode.EOF, (node as EvalInputNode).eofNode.tokenKind)
     }
+
+    @Test
+    fun testAtomSetLiteralSingleStarExpr() {
+        val tokens = arrayOf(
+            Pair(Token(TokenCode.PyLeftCurly), 0),
+            Pair(Token(TokenCode.PyMul), 1),
+            Pair(NameToken(2, 3, "a"), 2),
+            Pair(Token(TokenCode.PyRightCurly), 4),
+            Pair(Token(TokenCode.EOF), 5)
+        )
+
+        val lexer = MockedPythonCoreTokenizer(tokens)
+        val parser = PythonCoreParser(lexer)
+        val node = parser.parseEvalInput()
+        assertEquals(true, (node is EvalInputNode))
+        assertEquals(true, (node as EvalInputNode).rightNode is SetNode)
+        assertEquals(0, ((node as EvalInputNode).rightNode as SetNode).nodeStartPos )
+        assertEquals(5, ((node as EvalInputNode).rightNode as SetNode).nodeEndPos )
+        assertEquals(TokenCode.PyLeftCurly, ((node as EvalInputNode).rightNode as SetNode).symbolOne.tokenKind )
+        assertEquals(true, ((node as EvalInputNode).rightNode as SetNode).rightNode is SetContainerNode )
+        assertEquals(1, (((node as EvalInputNode).rightNode as SetNode).rightNode as SetContainerNode ).nodeStartPos)
+        assertEquals(4, (((node as EvalInputNode).rightNode as SetNode).rightNode as SetContainerNode ).nodeEndPos)
+        assertEquals(0, (((node as EvalInputNode).rightNode as SetNode).rightNode as SetContainerNode ).elementerSeparators!!.size)
+        assertEquals(1, (((node as EvalInputNode).rightNode as SetNode).rightNode as SetContainerNode ).elementNodes.size)
+        assertEquals(true, (((node as EvalInputNode).rightNode as SetNode).rightNode as SetContainerNode ).elementNodes[0] is BitwiseStarExpressionNode)
+        assertEquals(TokenCode.PyRightCurly, ((node as EvalInputNode).rightNode as SetNode).symbolTwo.tokenKind )
+        assertEquals(0, (node as EvalInputNode).newlineNode.size)
+        assertEquals(TokenCode.EOF, (node as EvalInputNode).eofNode.tokenKind)
+    }
 }
